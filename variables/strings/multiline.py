4f1ad8a506ee3c86648a20dd4bcf28318cf1d393
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-10-06 12:49:16 +0000

    06/10

---
+++ variables/strings/multiline.py
@@ -0,0 +1,6 @@
+String = """Once a string is created, 
+we cannot change it. 
+Any operation in terms of modifying a 
+string will actually make a new string"""
+
+print("Multiline:", String)
---
+++ variables/strings/string.py
@@ -0,0 +1,5 @@
+Basic_String = "python programming"
+ 
+print("String:", Basic_String)
+
+print("Data Type:", type(Basic_String))
