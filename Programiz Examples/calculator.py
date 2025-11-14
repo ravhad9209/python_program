ac343fb4e509ad2d945fd15391dc81ace33f989a
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-11-14 12:54:22 +0000

    14/11/25

---
+++ Programiz Examples/calculator.py
@@ -0,0 +1,20 @@
+def add (a , b):
+    return a+b
+
+def subtract (a , b):
+    return a-b
+
+def multiply (a , b):
+    return a*b
+
+def divide (a , b):
+    return a/b
+
+print("selected oprations.")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+
+while True:
+    Choice = input("Enter choice (1/2/3/4)")
---
+++ Programiz Examples/simple_cal.py
@@ -0,0 +1,26 @@
+## user defined
+# def add_number(a,b):
+#     sum = (a+b)
+#     print("number add:",sum)
+# a = int(input("enter the number 1 ="))
+# b = int(input("enter the number 2 ="))
+# add_number(a,b)
+
+## function arguments
+# def subtract_number(a, b):
+#     sum = (a -b)
+#     print("subtraction = ", sum)
+# subtract_number(10,20)
+
+##user defined
+# def multiply_number(a , b):
+#     sum = (a*b)
+#     print("multiply two numbers:", sum)
+# a = int(input("Enter the first number = "))
+# b = int(input("Enter second number = ")) 
+# multiply_number(a,b)
+
+#function argument
+def divide_number(a , b):
+    print("divide numbers :", (a/b))
+divide_number(18,3)
