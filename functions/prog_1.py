d988a6ec54f6bb99afaef9ff2ac44839bc18a54c
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-11-17 12:41:34 +0000

    17/11/25

---
+++ functions/prog_1.py
@@ -0,0 +1,28 @@
+# abs functions
+integer = -29
+print(abs(integer))
+
+integer_2 = 36
+print(abs(integer_2))
+
+# all functions
+
+new_value = [0,False, 23]
+print(all(new_value))
+
+new_1 = [False , 54]
+print(all(new_1))
+
+new = [34, -32]
+print(all(new))
+
+#bin function
+a = 54
+print(bin(a))
+
+# bool function
+abc = []
+print(bool(abc))
+
+abcd = [23]
+print(bool(abcd))
