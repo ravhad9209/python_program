fac6eb429912304d6165a23e5561a3f3cd59e60b
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-11-18 12:27:42 +0000

    18/11/25

--- functions/prog_1.py
+++ functions/prog_1.py
@@ -1,28 +1,55 @@
-# abs functions
-integer = -29
-print(abs(integer))
+# # abs functions
+# integer = -29
+# print("remove negetive sign",abs(integer))
 
-integer_2 = 36
-print(abs(integer_2))
+# integer_2 = 36
+# print(abs(integer_2))
+# print("_"*30)
 
-# all functions
+# # all functions
 
-new_value = [0,False, 23]
-print(all(new_value))
+# new_value = [0,False, 23]
+# print(all(new_value))
 
-new_1 = [False , 54]
-print(all(new_1))
+# new_1 = [False , 54]
+# print(all(new_1))
 
-new = [34, -32]
-print(all(new))
+# new = [34, -32]
+# print(all(new))
 
-#bin function
-a = 54
-print(bin(a))
+# #bin function
+# a = 54
+# print(bin(a))
 
-# bool function
-abc = []
-print(bool(abc))
+# # bool function
+# print("--- give values true or false ---")
+# abc = []
+# print(bool(abc))
 
-abcd = [23]
-print(bool(abcd))
+# abcd = [23]
+# print(bool(abcd))
+
+# # callable function 
+
+# a = 12
+# print("check fuction or not = ",callable(a))
+
+#compile code
+code_str = 'a=7\nb=5\nprint("sum = ",a+b)'
+code = compile(code_str,'sum.py','exec')
+print(type(code))
+exec(code)
+# exec(a)
+
+#exec() function
+y = 13
+exec('print(y==13)')
+exec('print(y+5)')
+
+#sum() function 
+x = sum([3,2,5])
+print(x)
+
+z = sum([4,3,5],15)
+print(z)
+
