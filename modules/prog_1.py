cf91b014d44ef4b399860759933897de4ecdcde4
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-11-22 12:37:09 +0000

    22/11/25

---
+++ modules/prog_1.py
@@ -0,0 +1,41 @@
+# #calculating square root 
+# import math
+# print(math.sqrt(5))
+
+# #calculating factorial of number
+# import math
+# a = int(input("Enter the number : "))
+# print(math.factorial(a))
+
+# #code compile and run 
+# import math
+# num = int(input())
+# exp = int(input())
+# run = math.pow(num,exp)
+# print(run)
+
+# #math modules in python 
+# import math 
+# dir(math)
+
+# #ceil()
+# import math 
+# n = eval(input())
+# # res = math.ceil(n)
+# # res = math.floor(n)
+# # res = math.fabs(n)
+# print(res)
+
+# # pi()
+# import math 
+# print(math.pi)
+
+#division concept
+a = int(input())
+b = int(input())
+res = a/b
+print("float division", res)
+run = a//b
+print("integer division", run)
+rvn = a%b
+print("Reminder", rvn)
