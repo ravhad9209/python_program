aa8b7a0047f8e63789c37b683f576e35f439dbb1
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-12-01 12:35:28 +0000

    01/12/25

---
+++ OOPs/inheritance.py
@@ -0,0 +1,32 @@
+# #single inheritance
+# class clg:
+#     def roll(self):
+#         print("103")
+
+# class tech(clg):
+#     def sir(self):
+#         print("Anand")
+
+# c = tech()
+# c.sir()
+# c.roll()
+
+# #muliple inheritance
+# class Grand_Father:
+#     def Abc(self):
+#         print("Grand Father")
+
+# class Father(Grand_Father):
+#     def Pqr(self):
+#         print("Father ")
+
+# class child(Father):
+#     def Xyz(self):
+#         print("Child")
+
+# F = child()
+# F.Xyz()
+# F.Pqr()
+# F.Abc()                 
+
+# 
