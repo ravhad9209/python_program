2d1f85e46a37395ca4a9e6d0727e225014925aa4
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-12-02 12:32:16 +0000

    02/12/25

--- OOPs/inheritance.py
+++ OOPs/inheritance.py
@@ -29,4 +29,26 @@
 # F.Pqr()
 # F.Abc()                 
 
-# 
+# (sub,sup)
+class cal:
+    def add(self, a, b):
+        return a+b;
+    
+class cal_2:
+    def mul(self, a, b):
+        return a*b;
+
+class divide(cal,cal_2):
+    def div(self,a,b):
+        return a/b;
+
+d = divide()
+# print(d.div(10,5))
+# print(d.mul(2,4))
+# print(d.add(10,10))    
+
+# print(issubclass(divide,cal_2))  
+# print(issubclass(cal,cal_2))      
+
+print(isinstance(cal,divide))
+print(isinstance(d,divide))
